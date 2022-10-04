02ec32a6a1d2f9bdd89bcf9640ae058db9da3e7d
Raihan adisatya <raihanadisatya@gmail.com>
2022-10-04 14:31:07 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/mobile_ziswaf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_ziswaf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryMain = const Color(0xff2680EB);
+Color primarySurface = const Color(0xffEBF3FF);
+Color primaryBorder = const Color(0xffD3E4F8);
+Color primaryHover = const Color(0xff0F6BD9);
+Color primaryPressed = const Color(0xff0A4182);
+Color primaryFocus = const Color.fromRGBO(38, 128, 235, .2);
+Color neutral10 = const Color(0xffFFFFFF);
+Color neutral20 = const Color(0xffF7F8F9);
+Color neutral30 = const Color(0xffF1F2F4);
+Color neutral40 = const Color(0xffE7E8EC);
+Color neutral50 = const Color(0xffD0D2DA);
+Color neutral60 = const Color(0xffB4B7C5);
+Color neutral70 = const Color(0xff9498AC);
+Color neutral80 = const Color(0xff8589A0);
+Color neutral90 = const Color(0xff6B718C);
+Color neutral100 = const Color(0xff42486C);
+Color secondaryMain = const Color(0xffF9AC50);
+Color dangerMain = const Color(0xffFC534A);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
+
+TextStyle textLRegular =
+    GoogleFonts.nunitoSans(fontWeight: regular, fontSize: 16);
+TextStyle textLSemibold =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 16);
+TextStyle textLBold = GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 16);
+TextStyle textLBlack = GoogleFonts.nunitoSans(fontWeight: black, fontSize: 16);
+TextStyle textMRegular =
+    GoogleFonts.nunitoSans(fontWeight: regular, fontSize: 14);
+TextStyle textMSemibold =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 14);
+TextStyle textMBold = GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 14);
+TextStyle textMBlack = GoogleFonts.nunitoSans(fontWeight: black, fontSize: 14);
+TextStyle textSRegular =
+    GoogleFonts.nunitoSans(fontWeight: regular, fontSize: 12);
+TextStyle textSSemibold =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 12);
+TextStyle textSBold = GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 12);
+TextStyle textSBlack = GoogleFonts.nunitoSans(fontWeight: black, fontSize: 12);
+TextStyle textXSRegular =
+    GoogleFonts.nunitoSans(fontWeight: regular, fontSize: 10);
+TextStyle textXSSemibold =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 10);
+TextStyle textXSBold = GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 10);
+TextStyle textXSBlack = GoogleFonts.nunitoSans(fontWeight: black, fontSize: 10);
+TextStyle pageTitleMedium =
+    GoogleFonts.nunitoSans(fontWeight: medium, fontSize: 20);
+TextStyle pageTitleBold =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 20);
+TextStyle listItemTitleRegular =
+    GoogleFonts.nunitoSans(fontWeight: regular, fontSize: 16);
+TextStyle listItemTitleSemiBold =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 16);
+TextStyle listItemTitleBold =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 16);
+TextStyle titleExtraBold = GoogleFonts.nunitoSans(
+    fontWeight: black, fontSize: 16, color: primaryPressed);
+TextStyle titlesuperBold =
+    GoogleFonts.nunitoSans(fontWeight: black, fontSize: 16, color: neutral100);
+TextStyle titleextraBold2 = GoogleFonts.nunitoSans(
+    fontWeight: extraBold, fontSize: 16, color: neutral100);
+TextStyle listItemTitleBlack =
+    GoogleFonts.nunitoSans(fontWeight: black, fontSize: 16);
+TextStyle listTitleRegular =
+    GoogleFonts.nunitoSans(fontWeight: regular, fontSize: 14);
+TextStyle listTitleSemiBold =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 14);
+TextStyle listTitleBold =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 14);
+TextStyle listTitleBlack =
+    GoogleFonts.nunitoSans(fontWeight: black, fontSize: 14);
+TextStyle paragraphTextRegular =
+    GoogleFonts.nunitoSans(fontWeight: regular, fontSize: 14);
+TextStyle paragraphTextSemiBold =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 14);
+TextStyle paragraphTextBold =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 14);
+TextStyle secondaryTextRegular =
+    GoogleFonts.nunitoSans(fontWeight: regular, fontSize: 14);
+TextStyle secondaryTextSemiBold =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 14);
+TextStyle secondaryTextBold =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 14);
+TextStyle titleTextMedium =
+    GoogleFonts.nunitoSans(fontWeight: medium, fontSize: 14);
+TextStyle titleTextBold =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 14);
+TextStyle titleExtraTextBold =
+    GoogleFonts.nunitoSans(fontWeight: extraBold, fontSize: 14);
+TextStyle buttonTabsTextMedium =
+    GoogleFonts.nunitoSans(fontWeight: medium, fontSize: 14);
+TextStyle buttonTabsTextBold =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 14);
+TextStyle captionTextRegular =
+    GoogleFonts.nunitoSans(fontWeight: regular, fontSize: 12);
+TextStyle captionTextSemiBold =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 12);
+TextStyle captionTextBold =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 12);
+TextStyle linkTextMedium =
+    GoogleFonts.nunitoSans(fontWeight: medium, fontSize: 12);
+TextStyle linkTextBold = GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 12);
+TextStyle overlineRegular =
+    GoogleFonts.nunitoSans(fontWeight: regular, fontSize: 10);
+TextStyle overlineSemiBold =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 10);
+TextStyle overlineBold =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 10, color: neutral80);
+TextStyle overlineBlack =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 10);
+TextStyle listNumber =
+    GoogleFonts.nunitoSans(fontWeight: bold, fontSize: 8, color: primaryMain);
+TextStyle overlineSemiBold2 =
+    GoogleFonts.nunitoSans(fontWeight: semiBold, fontSize: 12);
