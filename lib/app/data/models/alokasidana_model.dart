f4e1e64d57673cc7c65c93c2fcef17ebecf6c5ac
NTSpace <nomuramtr75@gmail.com>
2022-11-21 19:54:39 +0700

    .

--- lib/app/data/models/alokasidana_model.dart
+++ lib/app/data/models/alokasidana_model.dart
@@ -15,7 +15,7 @@ class Alokasidana {
   Alokasidana.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     transaksiId = json['transaksi_id'];
-    jenisDonasi = json['jenisdonasis'];
+    jenisDonasi = json['jenisDonasis'];
     nominal = json['nominal'];
     dihapus = json['dihapus'];
   }
