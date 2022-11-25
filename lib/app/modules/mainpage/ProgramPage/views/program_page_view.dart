6f5a595bf702ae1135c35f959c4f2a9ed75eae76
Raihan adisatya <raihanadisatya@gmail.com>
2022-11-25 15:47:27 +0700

    fix bug

--- lib/app/modules/mainpage/ProgramPage/views/program_page_view.dart
+++ lib/app/modules/mainpage/ProgramPage/views/program_page_view.dart
@@ -21,143 +21,160 @@ class ProgramPageView extends GetView<ProgramPageController> {
     controller.getPrograms2();
     controller.gettotaldana();
 
-    return Obx(
-      () => Scaffold(
-          backgroundColor: const Color(0xffECF7F4),
-          body: controller2.user.value!.status != 1
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return Obx(() => controller2.user.value!.status != 1
+        ? Scaffold(
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                SizedBox(
+                  height: 100,
+                ),
+                Center(child: CircularProgressIndicator()),
+              ],
+            ),
+          )
+        : Scaffold(
+            backgroundColor: const Color(0xffECF7F4),
+            body: RefreshIndicator(
+              onRefresh: () async {
+                controller.getPrograms();
+                controller.refreshProgram();
+                controller.gettotaldana();
+              },
+              child: SingleChildScrollView(
+                child: Stack(
                   children: [
-                    const SizedBox(
-                      height: 91,
-                    ),
-                    Center(
-                      child: Image.asset(
-                        'assets/images/nonaktif.png',
-                        width: 288,
-                        height: 288,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: Text(
-                        'Akun Anda\nDinonaktifkan',
-                        style: h2ExtraBold.copyWith(color: neutral100),
+                      padding: const EdgeInsets.only(top: 44),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Image.asset(
+                          'assets/images/bagmoney.png',
+                          width: 163.26,
+                          height: 163.26,
+                        ),
                       ),
                     ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: Text(
-                        'Silahkan hubungi admin untuk mengaktifkan\nakun Anda kembali',
-                        style: textLSemibold.copyWith(color: neutral80),
-                      ),
-                    ),
-                  ],
-                )
-              : RefreshIndicator(
-                  onRefresh: () async {
-                    controller.getPrograms();
-                    controller.refreshProgram();
-                    controller.gettotaldana();
-                  },
-                  child: SingleChildScrollView(
-                    child: Stack(
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Padding(
-                          padding: const EdgeInsets.only(top: 44),
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Image.asset(
-                              'assets/images/bagmoney.png',
-                              width: 163.26,
-                              height: 163.26,
-                            ),
+                          padding: const EdgeInsets.only(left: 16, top: 68),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Dana Terkumpul',
+                                  style: listTitleSemiBold.copyWith(
+                                      color: neutral90)),
+                              const SizedBox(
+                                height: 2,
+                              ),
+                              Obx(
+                                () => Text(
+                                    NumberFormat.currency(
+                                      locale: 'id',
+                                      name: 'Rp',
+                                      decimalDigits: 0,
+                                    ).format(ProgC.totalDana.value!
+                                            .totalDanaProgram ??
+                                        0),
+                                    style: pageTitleBold.copyWith(
+                                        color: primaryHover)),
+                              ),
+                            ],
                           ),
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 16, top: 68),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text('Dana Terkumpul',
-                                      style: listTitleSemiBold.copyWith(
-                                          color: neutral90)),
-                                  const SizedBox(
-                                    height: 2,
-                                  ),
-                                  Obx(
-                                    () => Text(
-                                        NumberFormat.currency(
-                                          locale: 'id',
-                                          name: 'Rp',
-                                          decimalDigits: 0,
-                                        ).format(ProgC.totalDana.value!
-                                                .totalDanaProgram ??
-                                            0),
-                                        style: pageTitleBold.copyWith(
-                                            color: primaryHover)),
-                                  ),
-                                ],
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.only(bottom: 24),
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20)),
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                color: Colors.transparent,
+                                child: MyTabBar(
+                                  controller: ProgC.tabController,
+                                  tabs: const [
+                                    Tab(text: 'Berlangsung'),
+                                    Tab(text: 'Selesai'),
+                                  ],
+                                ),
                               ),
-                            ),
-                            const SizedBox(
-                              height: 24,
-                            ),
-                            Container(
-                              width: double.infinity,
-                              padding: const EdgeInsets.only(bottom: 24),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(20),
-                                    topRight: Radius.circular(20)),
-                              ),
-                              child: Column(
-                                children: [
-                                  Container(
-                                    color: Colors.transparent,
-                                    child: MyTabBar(
+                              const SizedBox(height: 26),
+                              RefreshIndicator(
+                                onRefresh: () async {
+                                  controller.getPrograms();
+                                },
+                                child: SizedBox(
+                                  width: double.maxFinite,
+                                  height: 720,
+                                  child: TabBarView(
                                       controller: ProgC.tabController,
-                                      tabs: const [
-                                        Tab(text: 'Berlangsung'),
-                                        Tab(text: 'Selesai'),
-                                      ],
-                                    ),
-                                  ),
-                                  const SizedBox(height: 26),
-                                  RefreshIndicator(
-                                    onRefresh: () async {
-                                      controller.getPrograms();
-                                    },
-                                    child: SizedBox(
-                                      width: double.maxFinite,
-                                      height: 720,
-                                      child: TabBarView(
-                                          controller: ProgC.tabController,
-                                          children: const [
-                                            ProgramBerlangsung(),
-                                            ProgramSudahSelesai(),
-                                          ]),
-                                    ),
-                                  ),
-                                ],
+                                      children: const [
+                                        ProgramBerlangsung(),
+                                        ProgramSudahSelesai(),
+                                      ]),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                )),
-    );
+                  ],
+                ),
+              ),
+            ),
+          ));
   }
 }
+
+
+
+
+// Scaffold(
+//               backgroundColor: const Color(0xffECF7F4),
+//               body: Column(
+//                 mainAxisAlignment: MainAxisAlignment.start,
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   const SizedBox(
+//                     height: 91,
+//                   ),
+//                   Center(
+//                     child: Image.asset(
+//                       'assets/images/nonaktif.png',
+//                       width: 288,
+//                       height: 288,
+//                       fit: BoxFit.cover,
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.only(left: 16),
+//                     child: Text(
+//                       'Akun Anda\nDinonaktifkan',
+//                       style: h2ExtraBold.copyWith(color: neutral100),
+//                     ),
+//                   ),
+//                   const SizedBox(
+//                     height: 24,
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.only(left: 16),
+//                     child: Text(
+//                       'Silahkan hubungi admin untuk mengaktifkan\nakun Anda kembali',
+//                       style: textLSemibold.copyWith(color: neutral80),
+//                     ),
+//                   ),
+//                 ],
+//               ))
